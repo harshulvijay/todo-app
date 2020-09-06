a739f5d5ed89931d89040478b58a1df450e053e2
harshulvijay <devhvijay@gmail.com>
2020-09-06 19:04:26 +0530

    remove unused references

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -10,55 +10,28 @@ import { waitFor } from '../../global/utils';
 })
 export class AppRoot {
   bg: HTMLDivElement;
-  a: HTMLDivElement;
 
   componentWillLoad() {
-    /* this.x.onkeydown = e => {
-      e.key === 'Enter' &&
-        (ThemeStore.themeID = +(e.target as HTMLInputElement).value);
-    }; * /
-    onThemeChange(`themeID`, id => {
-      if (!isNaN(id)) {
-        const { wallpaper } = Themes[id];
-        this.bg.style.setProperty(
-          `--app-bg`,
-          `url('${wallpaper}')` /* `linear-gradient(to bottom right, #cc2b5e, #753a88)` * /,
-        );
-      }
-    });*/
     onThemeChange(`themeID`, async id => {
       if (+ThemeStore.currentTheme !== +id) {
         this.bg.style.opacity = '0';
         await waitFor(150);
         const { wallpaper } = Themes[+id];
-        this.bg.style.setProperty(
-          `--app-bg`,
-          `url('${wallpaper}')`,
-        );
+        this.bg.style.setProperty(`--app-bg`, `url('${wallpaper}')`);
         ThemeStore.currentTheme = [+id];
         await waitFor(150);
         this.bg.style.opacity = '1';
       }
     });
-    /* onListsStoreChange(`currentList`, id => {
-      const themeID = ListStore.lists[id].meta.theme;
-      if (ThemeStore.currentTheme !== themeID) {
-        const { wallpaper } = Themes[themeID];
-        this.bg.style.setProperty(`--app-bg`, `url('${wallpaper}')`);
-        ThemeStore.currentTheme = themeID;
-      }
-    }); */
   }
 
   componentDidLoad() {
     ThemeStore.themeID = [1];
   }
 
-  /* evt.shiftKey && this.bg.style.setProperty(`--app-bg`, `url('/assets/bg.png')`) */
-
   render() {
     return (
-      <div class="content" ref={t => (this.a = t)}>
+      <div class="content">
         <div class="nav">
           <app-nav></app-nav>
         </div>
