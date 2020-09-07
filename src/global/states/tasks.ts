c0c032a19f556405b039f6b113778b53ad6379e3
harshulvijay <devhvijay@gmail.com>
2020-09-07 11:24:10 +0530

    - some refactoring - add loading of all tasks upon the creation of `TasksStore`

--- src/global/states/tasks.ts
+++ src/global/states/tasks.ts
@@ -1,5 +1,6 @@
 import { createStore } from '@stencil/store';
 import { Task } from '../interfaces/task';
+import { getTaskIDs, readTask } from '../db.worker';
 
 type TasksObject = {
   tasks: {
@@ -7,11 +8,23 @@ type TasksObject = {
   };
 };
 
-/* const { state } = createStore<TasksObject>({
-  tasks: {},
-}); */
+const load = async () => {
+  const data: { [key: string]: Task } = {};
+  const ids = await getTaskIDs();
+  for await (const taskID of ids) {
+    const list = await readTask(taskID as string);
+    data[list.meta._id] = list;
+  }
+  return data;
+};
 
-export const taskStore = createStore<TasksObject>({
+const { state, onChange } = createStore<TasksObject>({
   tasks: {},
 });
-export const taskState = taskStore.state;
+
+// Load the data that's already stored in IndexedDB into memory
+void (async function () {
+  state.tasks = await load();
+})();
+
+export { state as TasksStore, onChange as onTaskStoreChange };
